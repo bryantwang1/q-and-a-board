a6ea1a0e12fd898a2d6b27ad530bc03f63368f21
Bryant Wang <bryant.m.wang@gmail.com>
2017-01-13 14:01:25 -0800

    Add code to new-answer.js to reset input fields after answer submitted, add condition to only save the answer if there is content in the answer input field.

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -10,14 +10,19 @@ export default Ember.Component.extend({
     saveAnswer() {
       var params = {
         user: this.get('user') || "anonymous",
-        answer: this.get('answer'),
+        answer: this.get('answer') || "none",
         time: new Date().toString(),
         upvotes: 0,
         downvotes: 0,
         question: this.get('question')
       };
+      this.set('user', '');
+      this.set('answer', '');
       this.set('addNewAnswer', false);
-      this.sendAction('saveAnswer', params);
+      if(params.answer !== "none")
+      {
+        this.sendAction('saveAnswer', params);
+      }
     }
   }
 });
