72b0d8b519c60405fb08404a41165e814a24ea15
Bryant Wang <bryant.m.wang@gmail.com>
2017-01-13 10:36:29 -0800

    Add delete functionality for individual questions.

--- app/routes/question.js
+++ app/routes/question.js
@@ -4,4 +4,12 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
   },
+  actions: {
+    delete(question) {
+      if(confirm("Are you sure you want to delete thie question and all of its answers?")) {
+        question.destroyRecord();
+        this.transitionTo("index");
+      }
+    }
+  }
 });
