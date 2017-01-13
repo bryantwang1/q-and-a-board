8af5864f3c997504ba2eca75635cc0f2ea5a4d5c
Bryant Wang <bryant.m.wang@gmail.com>
2017-01-13 11:00:45 -0800

    Add ability to save new questions.

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+    saveQuestion() {
+      var params = {
+        user: this.get('user'),
+        question: this.get('question'),
+        time: new Date().toString()
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion', params);
+    }
+  }
 });
